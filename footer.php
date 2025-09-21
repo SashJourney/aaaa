2773359259916e82b2d94021a413bc2d7c86b4e4
SashJourney <sashjourney69@yahoo.com>
2025-09-21 10:07:37 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,12 @@
 <footer>
-    <p>&copy; <?php echo date('Y'); ?> Hackernull Theme</p>
+  <h2>Subscribe to our Newsletter</h2>
+  <form action="#" method="post">
+    <input type="email" placeholder="Enter your email" required>
+    <button type="submit">Subscribe</button>
+  </form>
+  <p>© <?php echo date('Y'); ?> Hackernull Theme</p>
 </footer>
+
 <?php wp_footer(); ?>
 </body>
 </html>
