b09a939fb1b206435e9b7ee93eb28339cd048644
SashJourney <sashjourney69@yahoo.com>
2025-09-21 10:09:02 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 60 },
+    "color": { "value": "#8ab4f8" },
+    "line_linked": { "enable": true, "color": "#8ab4f8" },
+    "move": { "speed": 2 }
+  }
+});
