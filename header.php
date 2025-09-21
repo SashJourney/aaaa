b2a41f55f5e2d8f396498f82d4f25021811add41
SashJourney <sashjourney69@yahoo.com>
2025-09-21 10:07:25 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
-    <?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
 <header>
-    <h1><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
-    <p><?php bloginfo('description'); ?></p>
+  <h1 class="site-title"><a href="<?php echo home_url(); ?>">HackerNull News</a></h1>
+  <p>Learn Ethical Hacking | Cybersecurity Tips & Trends</p>
 </header>
+
+<div id="particles-js"></div>
