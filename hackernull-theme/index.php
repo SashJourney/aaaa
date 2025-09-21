0a86d0e5aec8ef22499c8e031d9d8fbb848c66cc
SashJourney <sashjourney69@yahoo.com>
2025-09-21 09:50:37 +0200

    Update index.php

--- hackernull-theme/index.php
+++ hackernull-theme/index.php
@@ -1,14 +1,2 @@
 <?php
-?><!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title><?php bloginfo('name'); ?></title>
-    <?php wp_head(); ?>
-</head>
-<body>
-    <h1>✅ Welcome to Hackernull Theme!</h1>
-    <p>If you see this, the theme is working 🎉</p>
-    <?php wp_footer(); ?>
-</body>
-</html>
+echo "✅ Hackernull theme is alive!";
